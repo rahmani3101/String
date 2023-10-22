cf32939088f90825f7b88bdfe357fcc026beb224
Mohammad Asad Rahmani <146559979+rahmani3101@users.noreply.github.com>
2023-10-22 22:21:48 +0530

    DateCalculator.java
    Design a program to accept a day number (between 1 and 366), year (in 4 digits) from the user to generate and display the corresponding date. Also, accept 'N' (1 <= N <= 100) from the user to compute and display the future date corresponding to 'N' days after the generated date. Display an error message if the value of the day number, year and N are not within the limit or not according to the condition specified.

--- DateCalculator.java
+++ DateCalculator.java
@@ -56,7 +56,7 @@ public class DateCalculator
         return sb.toString();
     }
     
-    public static void main(String args[]) {
+    public static void main() {
         Scanner in = new Scanner(System.in);
         System.out.print("DAY NUMBER: ");
         int dayNum = in.nextInt();
