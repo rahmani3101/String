2d5455f1c53505517feb1b204346f7a34461e1ed
Mohammad Asad Rahmani <146559979+rahmani3101@users.noreply.github.com>
2023-11-24 23:31:58 +0530

    Create encrytByNChar.java

---
+++ encrytByNChar.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+class Encrypt 
+{
+    static void main()
+    {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter a sentence ");
+        String str=in.nextLine();
+        System.out.println("Enter number of char to shift ");
+        int n =in.nextInt();
+        
+        String key ="";
+        for(int i=0;i<str.length();i++)
+        {
+            char ch=str.charAt(i);
+            if(ch==' ' )
+                ch=' ';
+            else if((ch>='A' && ch<='Z'-n) || (ch>='a' && ch<='z'-n ))
+                ch=(char)(ch+n);
+            else if((ch>='Z'-n+1 && ch<='Z')    || (ch>='z'-n+1 && ch<='z')  )
+                ch=(char)(ch-23);
+            key+=ch;
+        }
+        System.out.println(key);
+    }
+}
